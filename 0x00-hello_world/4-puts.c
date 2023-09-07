71f85f04a2331c060dec306ff8c31032a44e075b
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-07 14:55:24 -0700

    created and modified puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void)
-/*running puts command*/
+/**
+ *main - Beginning
+ *
+ * Return: Always 0 (success)
+ * */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
