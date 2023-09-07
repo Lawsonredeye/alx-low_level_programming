f38a69d06d9bccf7bebdff399ed41a2f490df46f
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-07 14:32:39 -0700

    created and modified 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/**
- *This is my first code on printf()
- */
+/*This is my first code*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
