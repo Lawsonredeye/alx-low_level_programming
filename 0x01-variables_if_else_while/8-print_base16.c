ca12d1e1590470e85c28954e35d702ed159619a5
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-09 09:55:11 -0700

    base 16 code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main- program that prints number of base 16
+ *  and it would run on lowercase
+ *  Return: return 0 and exit from the program
+ */
+int main(void)
+{
+	int bigBase;
+
+	for (bigBase = 0; bigBase < 16; bigBase++)
+		if (i < 10)
+		{
+			putchar('0' + bigBase);
+		} else
+		{
+			putchar('a' + (i - 10));
+		}
+	putchar('\n');
+	return (0);
+}
