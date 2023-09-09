7a4c6c83357de53baf4c5b28c2a4f11a2add3a00
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-09 10:36:33 -0700

    another combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - display possible combp
+ * of two digit-numbers
+ * Return: return 0 and exit
+ */
+int main(void)
+{
+	int p1, p2;
+
+	for (p1 = 0; p1 <= 99; pn1++)
+	{
+		for (p2 = 0; p2 <= 99; p2++)
+		{
+			if (p1 < p2 && p1 != p2)
+			{
+				putchar((p1 / 10) + '0');
+				putchar((p1 % 10) + '0');
+				putchar(' ');
+				putchar((p2 / 10) + '0');
+				putchar((p2 % 10) + '0');
+				if (p1 != 98 || p2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
