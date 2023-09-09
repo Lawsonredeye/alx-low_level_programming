b03412e81d36b6f10ee6932df8bcf4e43f562daa
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-09 08:42:19 -0700

    used putchar for coding

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - creating a program for printing
+ *  digits from 0 - 10
+ *  Return: retuirn 0 and exit program
+ */
+int main(void)
+{
+	int myNum
+		;
+	for (myNum = 0; myNum <= 10; myNum++)
+		putchar(myNum);
+	putchar('\n');
+
+	return (0);
+}
