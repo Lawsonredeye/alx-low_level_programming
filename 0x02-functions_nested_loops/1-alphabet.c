93960e56a376a0d68cd37a7f6b83dfd7f17cba78
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-12 14:12:34 -0700

    remodified the file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - this is used to ease the stress
+ * of writing longer codes with a function
+ * Return: return 0 for success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
