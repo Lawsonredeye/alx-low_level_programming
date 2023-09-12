5dba1cc0c88f8cb20a7ba5030cd3fbd34f01f067
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-12 16:23:24 -0700

    used putchar to print alphabet strings

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,11 @@
  * of writing longer codes with a function
  * Return: return 0 for success
  */
-void print_alphabet(void){
-    int stand;
-    for (int stand = 97; stand <= 122; stand++)
-        putchar(stand);
-}
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	int stand;
+
+	for (int stand = 97; stand <= 122; stand++)
+		putchar(stand);
+	return (0);
 }
