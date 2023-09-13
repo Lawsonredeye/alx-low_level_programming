9fc34650c720a78b3c0e02d85aef471ed15e6ecc
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-13 10:23:44 -0700

    modified and edited this file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - this is used to ease the stress
- * of writing longer codes with a function
- * Return: return 0 for success
+ * print_alphabet - prints the alphabet, in
+ * lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	int stand;
-
 	for (int stand = 97; stand <= 122; stand++)
+	{
 		_putchar(stand);
+	}
+		_putchar('\n');
 }
