6b17fe714eccdca72e16e4f47de1bc4ca05390bb
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-15 15:41:04 -0700

    hot fixes and new execution

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 -9
+ * Return: returns 0
+ */
+void print_numbers(void)
+{
+	for (int print_numbers = 0; print_numbers <= 9; print_numbers++)
+	{
+		_putchar('0' + 1);
+	}
+	_putchar(''\n')
+}
