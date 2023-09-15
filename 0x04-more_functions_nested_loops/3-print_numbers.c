98b2ded33bf0396f0cbbe8a82252720dd524c754
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-15 15:53:05 -0700

    hot fixes and new execution

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,11 @@
  */
 void print_numbers(void)
 {
-	for (int print_numbers = 0; print_numbers <= 9; print_numbers++)
+	int print_numbers;
+
+	for (print_numbers = 0; print_numbers <= 10; print_numbers++)
 	{
 		_putchar('0' + 1);
 	}
-	_putchar(''\n')
+	_putchar('\n');
 }
