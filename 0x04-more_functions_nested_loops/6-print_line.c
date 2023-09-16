3a550cfdf0671e11bea1f03652b873c6f87852ee
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-15 17:52:12 -0700

    hot fixes and new execution

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: the parameer that holds the value
+ * Return: return 0 for success
+ */
+void print_line(int n)
+{
+	int n;
+	int line;
+
+	for (int line = 0; line < n; line++)
+	{
+		putchar('95');
+	}
+	return (0);
+}
