0b57e457bea67716b506fa1f08ca442ba012299e
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-18 15:20:06 -0700

    made a program that swaps values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: agrument 1
+ * @b: argument 2
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
