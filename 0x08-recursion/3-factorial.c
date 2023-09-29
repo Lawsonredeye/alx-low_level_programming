a064dd67fa377f4f23875326c10ba9562f49b998
lawsonredeye <omoregbeeolawson@gmail.com>
2023-09-29 06:24:51 -0700

    fixed the issues on the if else statement

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,7 +8,7 @@
  */
 int factorial(int n)
 {
-	if (n < 0)
+	if ( n < 0)
 	{
 		return (-1);
 	}
@@ -16,7 +16,7 @@ int factorial(int n)
 	{
 		return (1);
 	}
-	else if (n > 0)
+	else
 	{
 		return (n * factorial(n - 1));
 	}
