49a7d1d1422444167c53e39a3146d206d0a64e49
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-11 13:38:23 -0700

    structure for a dog information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure for dog profile
+ * @name: name placeholder
+ * @age: age placeholder
+ * @owner: owner placeholder
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
