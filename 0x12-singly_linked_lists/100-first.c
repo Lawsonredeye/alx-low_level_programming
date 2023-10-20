61753810051f3e7afbccb4d9655306a6864f6009
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-20 15:32:55 -0700

    revamped the code by removing the attribute from the function definition source code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * easyFunc - prints a statement before the main function
  * Return: returns (0)
  */
-__attribute__ ((constructor)) void easyFunc(void)
+void easyFunc(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon myback!\n");
