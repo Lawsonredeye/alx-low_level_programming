9e240d7514974eb006f56ef8da15047dcad07c68
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-23 12:47:34 -0700

    qick code fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_listint - function that frees listint_t
+ * @head: pointer to all node
+ * Return: void
+ */
 void free_listint(listint_t *head)
 {
 	int i;
