5868dc36662dc9c3a5adc7f8d0fc9c74aa3f99b0
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-31 15:44:20 +0100

    [fixed]removed printf for debugging errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,13 +18,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		printf ("a");
 		return (-1);
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
-		printf ("b");
 		return (-1);
 	}
 	if (text_content == NULL)
@@ -32,7 +30,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	byte_write = write(fd, buffer, strlen(text_content));
 	if (byte_write == -1)
 	{
-		printf("c");
 		close(fd);
 		return (-1);
 	}
