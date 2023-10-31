6809ecfc23d7ecdc852462af69965c24bbed5151
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-31 08:20:38 +0100

    added spaces

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename,O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
@@ -36,7 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return  (0);
 	}
-	byte_write = write(STDOUT_FILENO,buff, byte_read);
+	byte_write = write(STDOUT_FILENO, buff, byte_read);
 	if (byte_write == -1 || byte_write != byte_read)
 	{
 		free(buff);
