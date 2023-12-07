ee28d1d8ec97715b15f3579fffd9b46056834521
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-07 23:42:27 +0100

    function that adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * _add_dnodeint - function that adds a new node
+ * add_dnodeint - function that adds a new node
  * at the beginning of a dlistint_t list.
  * @head: head node to be linked with
  * @n: data value of the new_node
