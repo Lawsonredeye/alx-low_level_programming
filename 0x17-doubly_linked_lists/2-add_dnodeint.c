1aecf8609b34d864756e430c0154238aa0a617ac
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-07 23:46:48 +0100

    turned new node into a pointer and fixed bugs

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * add_dnodeint - function that adds a new node
@@ -10,7 +11,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_d new_node;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -18,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_mode->prev = NULL;
+	new_node->prev = NULL;
 	new_node->next = *head;
 	if (*head != NULL)
 		(*head)->next = new_node;
