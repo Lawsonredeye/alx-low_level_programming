293ac57705746a03649c75359e447adaea78a86d
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-08 14:54:54 +0100

    added betty coding style

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+*  delete_dnodeint_at_index - deletes the node at index
+*  index of a dlistint_t linked list.
+*  @head: list to be searched
+*  @index: point which we want to delete
+*  Return: 1 to show success
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
