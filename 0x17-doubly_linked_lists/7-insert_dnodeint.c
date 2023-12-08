0cc6bc0bf75ff1860e7401207c16ff11a3f45d82
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-08 12:04:53 +0100

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_node->next = current->next;
 				if (current->next != NULL)
 					current->next->prev = new_node;
+				current->next = new_node;
 				return (new_node);
 			}
 			current = current->next;
