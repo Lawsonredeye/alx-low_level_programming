4aab85a85cd92f3d6bbb2db1cfe393e593c3c7b6
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-25 02:15:21 +0100

    fixed codestyle format

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,27 +4,27 @@
 /**
  * hash_table_create - function that creates a hash table
  * @size: size of the array
- * Return: a pointer to the newly created hash function 
+ * Return: a pointer to the newly created hash function
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	int i;
 	hash_table_t *table;
 
-        table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
-        if (table == NULL)
-            return (NULL);
-        table->size = size;
-        table->array = malloc(size * sizeof(hash_table_t *));
-        if (table->array == NULL)
-        {
-            free(table);
-            return (NULL);
-        }
-        for (i = 0; i < size; i++)
-        {
-                table->array[i] = NULL;
-        }
-        return (table);
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_table_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
