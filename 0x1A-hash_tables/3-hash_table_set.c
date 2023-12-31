941f9b4384e7f9c9b38fb3bc71d74b150fed76d5
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-31 21:18:39 +0100

    implemented small tweaks to the coding style and also removed the else statement after the ht == NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,43 +12,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *p;
+
 	if (key == NULL || value == NULL)
 		return (0);
 	if (ht == NULL)
 		return (0);
+	p = malloc(sizeof(hash_node_t));
+	if (p == NULL)
+		return (0);
+	p->key = malloc((strlen(key) + 1) * sizeof(char));
+	p->value = malloc((strlen(value) + 1) * sizeof(char));
+	if (p->key == NULL || p->value == NULL)
+	{
+		free(p);
+		return (0);
+	}
+	if (strdup(key) == NULL || strdup(value) == NULL)
+	{
+		free(p);
+		return (0);
+	}
+	p->key = strdup(key);
+	p->value = strdup(value);
+	if (p->key == NULL || p->value == NULL)
+	{
+		free(p);
+		return (0);
+	}
+	index = hash_djb2((unsigned char *)p->key);
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = p;
 	else
 	{
-		p = malloc(sizeof(hash_node_t));
-		if (p == NULL)
-			return (0);
-		p->key = malloc((strlen(key) + 1) * sizeof(char));
-		p->value = malloc((strlen(value) + 1) * sizeof(char));
-		if (p->key == NULL || p->value == NULL)
-		{
-			free(p);
-			return (0);
-		}
-		if (strdup(key) == NULL || strdup(value) == NULL)
-		{
-			free(p);
-			return (0);
-		}
-		p->key = strdup(key);
-		p->value = strdup(value);
-		if (p->key == NULL || p->value == NULL)
-		{
-			free(p);
-			return (0);
-		}
-		index = hash_djb2((unsigned char *)p->key);
-
-		if (ht->array[index] == NULL)
-			ht->array[index] = p;
-		else
-		{
-			p->next = ht->array[index];
-			ht->array[index] = p;
-		}
+		p->next = ht->array[index];
+		ht->array[index] = p;
 	}
 	return (1);
 }
