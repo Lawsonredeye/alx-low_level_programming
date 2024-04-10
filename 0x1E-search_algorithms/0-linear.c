881907fdddffdaf304608a4aa74bd9656b2d7fa1
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-10 20:19:17 +0100

    removed the sixe

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,11 +10,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i, j = size;
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < j; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
