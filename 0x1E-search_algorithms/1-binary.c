020af3aa1399fdbd95e438c8caed6cb82a85c60e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-10 15:55:22 +0100

    complexity

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+int binary_search(int *array, size_t size, int value)
+{
+    int M, i = 0, L = 0, R = size - 1;
+
+    for (i = 0; L <= R; i++)
+    {
+        M = (L + R) / 2;
+        if (array[M] == value)
+            return (M);
+        else if (array[M] > value)
+            R = M - 1;
+        else if (array[M] < value)
+            L = M + 1;
+    }
+    return (-1);
+}
