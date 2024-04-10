1654f7ca4bd7c64dd00b52637046a5459bf5fce1
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-10 20:20:30 +0100

    reverted

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,11 +10,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i, j = size;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < j; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
