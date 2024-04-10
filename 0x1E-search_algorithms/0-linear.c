a6fcb7e59a011ddd8e70cac772a634f9345485e3
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-10 14:12:12 +0100

    a script that performs linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that perfroms linear search by taking in value
+ * an array pointer and the size of the array
+ * @array: pointer to the first element of the array
+ * @size: size of the array
+ * @value: value to be searched for within the array
+ * Return: The index of where the array is located or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (array == NULL)
+        return (-1);
+    for (i = 0; i < size - 1; i++)
+    {
+        if (array[i] == value)
+            return (i);
+        printf("Value checked array[%d] = [%d]", i, array[i]);
+    }
+    return (-1);
+}
