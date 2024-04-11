93c793a1b8b7e45ebca1626535b2166507c23177
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-11 12:57:08 +0100

    jump sort technique

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - function that searches for a value in a sorted
+ * array of integers using the Jump search algorithm
+ * @array: pointer to the first element of the array
+ * @size: size of the array
+ * @value: value to be searched for within the array
+ * Return: The index of where the array is located or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int block = 0;
+	int i = 0, j;
+	int sizes = (int)size;
+
+	while (block < sizes)
+	{
+		if (array[i] == value)
+			return (i);
+		i = block;
+		if (array[i] > value)
+		{
+			for (j = i; i > value; j--)
+			{
+				if (array[j] == value)
+					return (j);
+			}
+		}
+		block += sqrt(sizes);
+	}
+	return (-1);
+}
